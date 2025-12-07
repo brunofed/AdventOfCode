9b7a00cc0ba5b19300208cc5648a2ef01bad5f87
Federici Bruno <bruno.federici@ai-esra.com>
2025-12-07 11:38:57 +0100

    06 cleaned

--- 2025/06/script.py
+++ 2025/06/script.py
@@ -1,57 +1,14 @@
-import math
-from collections import Counter, defaultdict
 from csv import reader
-from dataclasses import dataclass
 from functools import reduce
-from itertools import pairwise
-from json import loads
-from operator import add, mul
-from os.path import dirname, join, realpath
 from pathlib import Path
-from typing import NamedTuple
 
-import networkx as nx
 import numpy as np
-import pandas as pd
 
 
 def read(filename):
     file_path = Path(__file__).parent / f"{filename}.txt"
     with open(file_path, "r") as file:
         return [row[0] for row in reader(file)]
-
-
-def advanced_read(
-    filename,
-    blank_rows=False,
-    rows_with_spaces=False,
-):
-    file_path = Path(__file__).parent / f"{filename}.txt"
-    with open(file_path, "r") as file:
-        if blank_rows:
-            grouped_rows = [[]]
-            idx = 0
-            for row in reader(file):
-                if row:
-                    grouped_rows[idx].append(row[0])
-                else:
-                    grouped_rows.append([])
-                    idx += 1
-            return grouped_rows
-        if rows_with_spaces:
-            lines = file.readlines()
-            return [line.rstrip() for line in lines]
-        return [row[0] for row in reader(file)]
-
-
-def str_to_ints(string, start_idx=0, spaces_are_meaningful=True):
-    if spaces_are_meaningful:
-        return apply(int, string.split()[start_idx:])
-    return int(string.replace(" ", "").split(":")[-1])  # only one number
-
-
-def is_within_bounds(coords, bounds):
-    return all(0 <= coords[i] < bounds[i] for i in [0, 1])
 
 
 ###### START OF ACTUAL CODE ######
@@ -93,7 +50,7 @@ def parse_input_str_2(inputs_str):
     return result
 
 
-def problem2(inputs):
+def problem2(inputs):  # ops, I did everything in the parsing step
     return inputs
 
 
